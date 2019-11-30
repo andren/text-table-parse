f102e37a4de60b9a3eeb919ceefda6b28718f97c
André Nascimento <40470926+andren@users.noreply.github.com>
2019-11-30 13:05:51 +0000

    added unit test for noSpace

--- src/test/test.spec.ts
+++ src/test/test.spec.ts
@@ -1,19 +1,132 @@
-import { hello } from "../main";
-import { expect } from "chai";
+import * as main from "../main";
+import * as fs from "fs";
+const expect = require("chai").expect;
 import "mocha";
 
-describe("First test", () => {
-    it("should return true", () => {
-        const result = hello();
-        expect(result).to.equal("Hello world!");
+describe("Testing outputTenantsNoSpace.txt", () => {
+    // Arrange
+    let testFileDir: string = "src\\test\\outputTenantsNoSpace.txt";
+    let testStream = fs.createReadStream(testFileDir);
+    let testCorrect_Table: main.TextTable = new main.TextTable();
+    testCorrect_Table.nCols = 3;
+    testCorrect_Table.nLins = 9;
+    
+    let testCorrect_Column: main.TextColumn = new main.TextColumn(0,37);
+    testCorrect_Column.label = "ID";
+    testCorrect_Column.startIndex = 0;
+    testCorrect_Column.endIndex = 37;
+    testCorrect_Column.colWidth = 37;
+    testCorrect_Column.lin = [ ,
+                               ,
+                               "aaaaaa11-bb22-c3c3-d44d-dsa8cjas0dja", 
+                               "sda89ca8-4aa3-4558-adbb-d3fc34631830",
+                               "v12avs23-4d25-4b16-agqd-a9a2t8d11e3a",
+                               "av12v12v-36f6-421a-ag25-34e41e53d2ta",
+                               "a125125n-76e1-4wb4-bac5-c83b2387f120",
+                               "bbbbbb22-09ge-41de-b212-b1t40ttet9e6"];
+    
+    let testCorrect_Column2: main.TextColumn = new main.TextColumn(37,34);
+    testCorrect_Column2.label = "Name";
+    testCorrect_Column2.startIndex = 37;
+    testCorrect_Column2.endIndex = 71;
+    testCorrect_Column2.colWidth = 34;
+    testCorrect_Column2.lin = [ ,
+                                ,
+                                "smallletters", 
+                                "Startcap",
+                                "this test spaces",
+                                "Some_NAME_HereInalternatingCases",
+                                "--startswith---endswith----",
+                                "'allthethingsarebetweenquotessss'"];
+
+    let testCorrect_Column3: main.TextColumn = new main.TextColumn(37,34);
+    testCorrect_Column3.label = "Roles";
+    testCorrect_Column3.startIndex = 71;
+    testCorrect_Column3.endIndex = NaN;
+    testCorrect_Column3.colWidth = undefined;
+    testCorrect_Column3.lin = [ ,
+                                ,
+                                "Administrator", 
+                                "Administrator",
+                                "Administrator",
+                                "Contributor",
+                                "Contributor",
+                                "Reader"];
+
+    testCorrect_Table.col.push(testCorrect_Column);
+    testCorrect_Table.col.push(testCorrect_Column2);
+    testCorrect_Table.col.push(testCorrect_Column3);
+
+    it("Should return textTable with right properties", async () => {
+        
+        // Act
+        const result = await main.parseTextTableToObjectAsync(testStream);
+        
+        // Assert
+        expect(result).to.eql(testCorrect_Table);
     });
 });
 
-// ID                                   Name                              Roles
-// --                                   ----                              -----
-// becedd69-8539-4d5e-a9f8-82d6569c6c4b cssiot                            Administrator
-// d343c263-4aa3-4558-adbb-d3fc34631800 Microsoft                         Administrator
-// f33183ab-4b45-4b16-a8cd-a9aa83d11e6a bentho test migration             Administrator
-// e663e40d-36a6-431a-a9f5-34e41e53d02a Health_POET_AzureSphere           Contributor
-// 4b2d2dcd-76e1-40b4-b785-c83b5a87fb40 --manisha-test-tenant             Contributor
-// 11a76bfd-099e-4cde-baa2-b40b9e0189e6 'devinwonTenantforAzureSphereLab' Reader
+describe("Testing outputTenants.txt", () => {
+    // Arrange
+    let testFileDir: string = "src\\test\\outputTenants.txt";
+    let testStream = fs.createReadStream(testFileDir);
+    let testCorrect_Table: main.TextTable = new main.TextTable();
+    testCorrect_Table.nCols = 3;
+    testCorrect_Table.nLins = 9;
+    
+    let testCorrect_Column: main.TextColumn = new main.TextColumn(0,37);
+    testCorrect_Column.label = "ID";
+    testCorrect_Column.startIndex = 0;
+    testCorrect_Column.endIndex = 37;
+    testCorrect_Column.colWidth = 37;
+    testCorrect_Column.lin = [ ,
+                               ,
+                               "aaaaaa11-bb22-c3c3-d44d-dsa8cjas0dja", 
+                               "sda89ca8-4aa3-4558-adbb-d3fc34631830",
+                               "v12avs23-4d25-4b16-agqd-a9a2t8d11e3a",
+                               "av12v12v-36f6-421a-ag25-34e41e53d2ta",
+                               "a125125n-76e1-4wb4-bac5-c83b2387f120",
+                               "bbbbbb22-09ge-41de-b212-b1t40ttet9e6"];
+    
+    let testCorrect_Column2: main.TextColumn = new main.TextColumn(37,34);
+    testCorrect_Column2.label = "Name";
+    testCorrect_Column2.startIndex = 37;
+    testCorrect_Column2.endIndex = 71;
+    testCorrect_Column2.colWidth = 34;
+    testCorrect_Column2.lin = [ ,
+                                ,
+                                "smallletters", 
+                                "Startcap",
+                                "this test spaces",
+                                "Some_NAME_HereInalternatingCases",
+                                "--startswith---endswith----",
+                                "'allthethingsarebetweenquotessss'"];
+
+    let testCorrect_Column3: main.TextColumn = new main.TextColumn(37,34);
+    testCorrect_Column3.label = "Roles";
+    testCorrect_Column3.startIndex = 71;
+    testCorrect_Column3.endIndex = NaN;
+    testCorrect_Column3.colWidth = undefined;
+    testCorrect_Column3.lin = [ ,
+                                ,
+                                "Administrator", 
+                                "Administrator",
+                                "Administrator",
+                                "Contributor",
+                                "Contributor",
+                                "Reader"];
+
+    testCorrect_Table.col.push(testCorrect_Column);
+    testCorrect_Table.col.push(testCorrect_Column2);
+    testCorrect_Table.col.push(testCorrect_Column3);
+
+    it("Should return textTable with right properties", async () => {
+        
+        // Act
+        const result = await main.parseTextTableToObjectAsync(testStream);
+        
+        // Assert
+        expect(result).to.eql(testCorrect_Table);
+    });
+});
